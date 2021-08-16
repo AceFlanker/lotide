279717e9368bbce16230596afb4e6c6060390714
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-16 16:05:32 -0700

    'tail' modified to include cases with empty arrays

--- tail.js
+++ tail.js
@@ -1,6 +1,10 @@
 // TAIL FUNCTION IMPLEMENTATION
 const tail = (arr) => {
-  return arr.slice(1);
+  if (arr.slice(1).length = 0) {
+    return [];
+  } else {
+    return arr.slice(1);
+  }
 };
                                                                                                                                  
 // TEST FUNCTION IMPLEMENTATION
@@ -8,6 +12,8 @@ const assertEqual = function(actual, expected) {
   if (Array.isArray(actual) === true) {
     if (actual.length !== expected.length) {
       console.log(`👎 Assertion Failed: ${actual} != ${expected} - unequal lengths`);
+    } else if (actual.length === 0) {
+      console.log(`💯 Assertion Passed: [] === []`);
     } else {
       for (let i = 0; i < actual.length; i++) {
         if (actual[i] !== expected[i]) {
@@ -32,3 +38,15 @@ const assertEqual = function(actual, expected) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(tail(words), ["Lighthouse", "Labs"]); 
+
+const words1 = [];
+tail(words1); // no need to capture the return value since we are not checking it
+assertEqual(tail(words1), []); 
+
+const words2 = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(tail(words), ["Heavyhouse", "Labs"]); 
+
+const words3 = [];
+tail(words1); // no need to capture the return value since we are not checking it
+assertEqual(tail(words1), [``]); 
