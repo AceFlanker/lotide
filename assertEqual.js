ba4ccacb4f18211a788c5e368d7d5126581b1cc0
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-16 16:07:06 -0700

    'assertEqual' modified to include cases with empty arrays

--- assertEqual.js
+++ assertEqual.js
@@ -3,6 +3,8 @@ const assertEqual = function(actual, expected) {
   if (Array.isArray(actual) === true) {
     if (actual.length !== expected.length) {
       console.log(`👎 Assertion Failed: ${actual} != ${expected} - unequal lengths`);
+    } else if (actual.length === 0) {
+      console.log(`💯 Assertion Passed: [] === []`);
     } else {
       for (let i = 0; i < actual.length; i++) {
         if (actual[i] !== expected[i]) {
