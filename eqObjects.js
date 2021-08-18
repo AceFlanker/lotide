1e770526bac7366868ac15ab6f9ba721a78b2b8d
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-18 15:47:00 -0700

    Fatal mistakes corrected, working flawlessly now

--- eqObjects.js
+++ eqObjects.js
@@ -44,22 +44,17 @@ const eqArrays = function(arr1, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  // let Bool = true;
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    for (const key in object1) {
-      if (Array.isArray(object1[key])) {
-        eqArrays(object1[key], object2[key]);
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        } else {
-          return true;
-        }
-      }
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;      
     }
   }
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
