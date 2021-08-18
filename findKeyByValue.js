e59c9043c77c7fbee587582305dbe6d54129ed9a
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-18 13:50:40 -0700

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (Array.isArray(actual) === true) {
+    if (actual.length !== expected.length) {
+      console.log(`👎 Assertion Failed: ${actual} != ${expected}`);
+    } else if (actual.length === 0) {
+      console.log(`💯 Assertion Passed: [] === []`);
+    } else {
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+          console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+          break;
+        }
+        if (actual[actual.length - 1] === expected[actual.length - 1]) {
+          console.log(`💯 Assertion Passed: ${actual} === ${expected}`);
+        }
+      }
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`💯 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+const findKeyByValue = function(inputObj, propValue) {
+  let keyName = undefined;
+  for (const key in inputObj) {
+    if (inputObj[key] === propValue) {
+      keyName = key;
+    }
+  }
+  return keyName;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
