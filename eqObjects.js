88daf2b44715d2ad53675ee6e739dc8819e1f80a
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-19 16:41:01 -0700

    'let' changed to 'const' on line 50

--- eqObjects.js
+++ eqObjects.js
@@ -47,7 +47,7 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  for (let key in object1) {
+  for (const key in object1) {
     if (Array.isArray(object1[key])) {
       if (!eqArrays(object1[key], object2[key])) { // This if statement blocks eqArrays from returning true
         return false;
