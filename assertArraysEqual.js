0aacce5cf336516bcb63529b7f7946039a333f04
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-24 07:09:30 -0700

    assertArraysEqual refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,10 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else if (arr1.length === 0) {
-    return true;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-      if (arr1[arr1.length - 1] === arr2[arr2.length - 1]) {
-        return true;
-      }
-    }
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   eqArrays(actual, expected) ? console.log(`💯 Assertion Passed: ${actual} === ${expected}`) : console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+module.exports = assertArraysEqual;
 
 assertArraysEqual([3, 4], [4, 3]);
 assertArraysEqual([1, 2], [1, 2, 3]);
